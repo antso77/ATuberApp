cc001821d1d37a33b6d4a0c3e8f8cc674761b036
antso77 <antso77@gmail.com>
2026-08-10 19:41:51 +0300

    Update config.js
    new channels

--- config.js
+++ config.js
@@ -6,7 +6,17 @@ const CONFIG = {
   channels: [
     { name: "LevelUPAnimations", id: "UCNK813T2BNBwcfDPDTPeMPA" },
     { name: "MadShenans", id: "UCEGOLGL2u5-HZdZXQahc3oA" },
-    { name: "EarthNowLive", id: "UChnIgJ8_PDy0AwV84XP9HTA" }
+    { name: "EarthNowLive", id: "UChnIgJ8_PDy0AwV84XP9HTA" },
+    {name: "Nate Ziller", id: "UC75sZDv6cuxARkRuouhHKwg"},
+    {name: "ArimuraTaishi", id: "UCw3PQjMe2Xjhs9fBBAhchkg"},
+    {name: "retrosutra", id: "UCo3gu5hwTBV5c2U3eR2ZxNg"},
+    {name: "SMG4 SHORTS", id: "UCUbslKd64OsMpBKhGJTNegg"},
+    {name: "Team Level UP", id: "UCdBU4IkJRKX6CRHPdxuofKw"},
+    {name: "JackNJellify", id: "UCeKLuqGciqZZ5RFYk5CbqXg"},
+    {name: "cookingWITHfred", id: "UCJFGFsECyWh0iHRVW2QQSkA"},
+    {name: "Brecht Pictures", id: "UCc0eytVGHZaOOvxuZ90aiPg"},
+    {name: "Pixi-Gags", id: "UC4cRiYSyYzw-rqFnGSTmw6w"},
+    {name: "petsasjim1", id: "UCm5AA3G5UYTYjohxC7If9Cg"}
   ],
 
   playlists: [
